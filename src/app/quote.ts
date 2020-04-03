df0f281554ee04f19e09c344ee09dc504f771a97
martingathu <martin5gathu@gmail.com>
2020-04-03 11:29:55 +0300

    add an array for quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     showDescription: boolean;
-    constructor(public id: number,public name: string,public author: string, public blogger:string,public postingDate: Date){
+    constructor(public id: number,public name: string,public author: string, public blogger:string,public postingDate: Date, public clickCounter: number, public clickCounter1: number){
       this.showDescription=false;
     }
   }
